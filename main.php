54d2b944693b9659d0f3429dff4a7860684ea173
Laks Gandikota <laks.gandikota@gmail.com>
2014-08-10 01:31:24 -0500

    first commit

---
+++ main.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> 
+<!-- The HTML 4.01 Transitional DOCTYPE declaration--> 
+<!-- above set at the top of the file will set     --> 
+<!-- the browser's rendering engine into           --> 
+<!-- "Quirks Mode". Replacing this declaration     --> 
+<!-- with a "Standards Mode" doctype is supported, --> 
+<!-- but may lead to some differences in layout.   --> 
+ 
+<html> 
+  <head> 
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+    <title>staturday.com</title> 
+    <style> 
+    body {
+	 font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	 padding: 100px;
+	}
+	
+	div {
+	/* basic */ 
+	 background-color: #fff;
+	 margin: 0 auto;
+	 width: 700px;
+	 padding: 50px;
+	 text-align: center;
+	/* border-radius */
+	 -webkit-border-radius: 5px;
+	 -moz-border-radius: 5px;
+	 border-radius: 5px;
+	/* box-shadow */
+	 -webkit-box-shadow: rgba(0,0,0,0.2) 0px 1px 3px;
+	 -moz-box-shadow: rgba(0,0,0,0.2) 0px 1px 3px;
+	 box-shadow: rgba(0,0,0,0.2) 0px 1px 3px;
+	}
+    </style> 
+
+
+  </head> 
+ 
+  <body> 
+    <div>
+    <pre>
+
+     _        _                 _                                  
+    | |      | |               | |                                 
+ ___| |_ __ _| |_ _   _ _ __ __| | __ _ _   _   ___ ___  _ __ ___  
+/ __| __/ _` | __| | | | '__/ _` |/ _` | | | | / __/ _ \| '_ ` _ \ 
+\__ \ || (_| | |_| |_| | | | (_| | (_| | |_| || (_| (_) | | | | | |
+|___/\__\__,_|\__|\__,_|_|  \__,_|\__,_|\__, (_)___\___/|_| |_| |_|
+                                         __/ |                     
+                                        |___/                      
+
+        </pre><br/><br/>&copy;<?php echo date("Y"); ?> Rebuttal Inc.</div> 
+  </body> 
+</html> 
